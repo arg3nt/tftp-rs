9b125f5fa38aaac512a4597a786f65e84069589b
arg3nt <nachera.dev@gmail.com>
2024-07-18 02:12:04 +0000

    Implement TFTP parser w/ basic tests

---
+++ src/main.rs
@@ -0,0 +1,152 @@
+// This is an implementation of a TFTP server compliant with RFC 1350
+
+
+// Transfer begins with request to read or write a file (also req for connection)
+// If server grants request, connection is opened, file is sent in blocks of 512 bytes.
+// Each data packet contains one block and must be acked before the next one is sent.
+//
+// On data loss, intended recipient times out and may retransmit last packet (data or ack),
+// causing sender to retransmit the lost packet.
+// Sender has to keep one packet for retransmission.
+//
+// Most errors cause connection to terminate. Err is signaled w/ error packet
+// This packet is unacked and never retransmitted.
+//
+// Timeouts are used to detect the case where the error packet gets lost.
+//
+// Errors caused by 3 types of events:
+// - unable to satisfy req (e.g. file not found, access violation)
+// - invalid packet
+// - loss of access to dependency (e.g. hard disk)
+//
+// Only one error condition that does not cause termination: when
+// source port of a received packet is incorrect.
+//
+// Protocol is implemented on top of UDP.
+//
+// Packet:
+//  - IP Header
+//      - UDP Header
+//          - TFTP header
+//              - remainder of packet (contents depend on packet type)
+//
+// TFTP uses src and destination port fields in UDP header, and length field represents
+// size of TFTP packet.
+//
+// Transfer identifiers (TIDs) used by TFTP are passed to Datagram layer and used as ports. Must be
+// between 0 and 65,535
+//
+// TFTP header is a 2-byte opcode field.
+//
+// ------------------------------
+// Initial connection protocol:
+//
+// transfer est by sending req and receiving a reply.
+// For read: send RRQ, receive first data packet
+// For write: send WRQ, recv ack
+//
+// Ack packet contains block number of the packet being acked. For write acks, block number is 0,
+// but ordinarily starts w/ 1 and increases.
+//
+// To create a conn, each end chooses a TID for itself randomly.
+//
+// Every packet is associated w/ src and dst TID. These become UDP ports.
+//
+// Requesting host chooses src TID and sends initial request to TID 69 (decimal)
+// Response uses a TID chosen by server as src and TID from requester as dst.
+//
+// Initial connection req can be duplicated by network, causing server to send 2 replies on
+// different TIDs. When this happens, client can send error packet for the second response
+// without disrupting its connection on the first response.
+//
+// -------------------------------
+// TFTP Packets
+//
+// opcode   operation
+// 1        Read req (RRQ)
+// 2        Write req (WRQ)
+// 3        Data (DATA)
+// 4        ACK
+// 5        ERROR
+//
+// RRQ/WRQ packet format:
+// 0x01/0x02 | Filename | 0x00 | Mode | 0x00
+//
+// Filename is a sequence of netascii bytes. Mode field contains "netascii", "octet", or "mail" (in
+// any combination of case).
+//
+// In netascii, host must translate data into its own format
+// In octet mode, file transfers in data format of the machine from which file is transferred.
+// If host receives octet file and then returns it, it must be identical to the original.
+//
+// DATA packet:
+// 0x03 | Block $ (2 bytes) | Data (0-512 bytes)
+//
+// - If block is 0-511 blocks long, it signals end of transfer.
+//
+// Sending a DATA packet is an acknowledgement of the ACK received for the previous data packet.
+//
+// WRQ and DATA packets acked by ACK or ERROR packets.
+// RRQ and ACK packets acked via DATA or ERROR packets.
+//
+// ACK packet format:
+// 0x04 | Block # (2 bytes)
+//
+// ERROR packet format:
+// 0x05 | ErrorCode (2 bytes) | ErrMsg | 0x00
+//
+// Error message is intended for human consumption and should be in netascii.
+//
+// ----------------------------
+// Normal termination
+//
+// End of transfer marked by DATA packet <512 bytes long. Packet should be acknowledged w/ ACK
+// packet. Whoever sends the final ACK is encouraged to wait for a while before terminating to
+// retransmit final ACK if it gets lost. Know it was lost if recv final DATA packet again.
+//
+// ----------------------------
+// Premature termination
+// An ERROR packet is sent as a courtesy, not retransmitted or ACKed.
+// Timeouts must also be used to detect errors.
+//
+// ----------------------------
+// Error codes
+//
+// 0    Not defined, see msg
+// 1    File not found
+// 2    Access violation
+// 3    Disk full / alloc exceeded
+// 4    Illegal TFTP op
+// 5    Unknown TID
+// 6    File already exists
+// 7    No such user
+//
+//
+// Server options:
+// - Disable writes
+// - Acceptable port range
+// - Bind IP address
+// - Timeout
+// - Filesystem prefix
+// - Max connections
+//
+
+use std::net::Ipv4Addr;
+
+pub mod tftp;
+
+use tftp::TftpSocket;
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("Hello world!");
+
+    let socket = TftpSocket::bind((Ipv4Addr::UNSPECIFIED, 69).into());
+
+
+
+    Ok(())
+}
+
---
+++ src/tftp.rs
@@ -0,0 +1,306 @@
+use async_io::Async;
+use std::error;
+use std::fmt;
+use std::io;
+use std::net::{UdpSocket, SocketAddr};
+use std::time::Duration;
+use tokio::time::timeout;
+use tokio::time::error::Elapsed;
+
+///////////////////////////////////////////////////////////////
+// Error-handling objects
+
+/// Represents an error returned from the TFTP Socket handler
+#[derive(Debug)]
+pub enum SocketError {
+    IO(io::Error),
+    PacketParse(String),
+    Timeout(Elapsed),
+}
+
+impl error::Error for SocketError {}
+
+impl fmt::Display for SocketError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SocketError::IO(e) => write!(f, "Socket IO error: {:#?}", e),
+            SocketError::PacketParse(e) => write!(f, "Packet parsing error: {:#?}", e),
+            SocketError::Timeout(e) => write!(f, "Socket IO timeout: {:#?}", e),
+        }
+    }
+}
+
+impl From<io::Error> for SocketError {
+    fn from(e: io::Error) -> Self {
+        SocketError::IO(e)
+    }
+}
+
+impl From<Elapsed> for SocketError {
+    fn from(e: Elapsed) -> Self {
+        SocketError::Timeout(e)
+    }
+}
+
+
+type TftpResult<T> = Result<T, SocketError>;
+
+/// Represents the mode for a file the client wishes to read or write.
+#[derive(Debug, PartialEq)]
+pub enum FileMode { NetAscii, Octet, Mail }
+
+/// Represents a TFTP Error code surfaced by a TFTP Error packet
+#[derive(Debug, PartialEq)]
+pub enum ErrorCode { Undefined, FileNotFound, AccessViolation, DiskFull, Illegal, UnknownTid, FileAlreadyExists, NoSuchUser }
+
+/// An enum representing a TFTP packet and its associated data.
+#[derive(Debug, PartialEq)]
+pub enum Packet {
+    /// A read request packet
+    ReadReq {
+        /// The file path the client wants to read.
+        path: String,
+
+        /// The file mode.
+        mode: FileMode,
+    },
+
+    /// A write request packet
+    WriteReq {
+        path: String,
+        mode: FileMode,
+    },
+
+    /// A data packet
+    Data {
+        /// The block number for this data packet.
+        block: u16,
+
+        /// The contents of the data itself.
+        data: Vec<u8>,
+    },
+
+    /// An acknowledgment packet
+    Ack {
+        /// The block being acknowledged.
+        block: u16,
+    },
+
+    /// An error packet.
+    Error {
+        code: ErrorCode,
+        message: String,
+    },
+}
+
+fn u16_from_buffer(buf: &[u8]) -> u16 {
+    (u16::from(buf[0]) << 8) + u16::from(buf[1])
+}
+
+/// Given a buffer, assumes the string begins at the beginning of the buffer and concatenates until
+/// it finds a 0 byte, which it assumes terminates the string.
+///
+/// Returns the string extracted from the buffer as well as the position of the 0 byte in the
+/// buffer it was given (or the size of the buffer, if no 0 byte was encountered)
+fn string_from_buffer(buf: &[u8]) -> (String, usize) {
+    let mut s = String::new();
+    for i in 0..buf.len() {
+        let c = buf[i];
+        if c == 0x00 {
+            return (s, i)
+        }
+        s.push(char::from(c));
+    }
+
+    (s, buf.len())
+}
+
+/// Utility function for obtaining the TFTP OpCode from a buffer
+#[derive(Debug, PartialEq)]
+pub enum OpCode { Rrq, Wrq, Data, Ack, Error }
+
+fn retrieve_op_code(buf: &[u8]) -> TftpResult<OpCode> {
+    let rawcode = u16_from_buffer(&buf[..2]);
+    match rawcode {
+        1 => Ok(OpCode::Rrq),
+        2 => Ok(OpCode::Wrq),
+        3 => Ok(OpCode::Data),
+        4 => Ok(OpCode::Ack),
+        5 => Ok(OpCode::Error),
+        _ => return Err(SocketError::PacketParse(
+            format!("Uknown opcode retrieved: {rawcode}").to_string())),
+    }
+}
+
+
+fn parse_path_and_mode(buf: &[u8]) -> TftpResult<(String, FileMode)> {
+    let (path, path_end) = string_from_buffer(&buf);
+
+    if path_end == buf.len() {
+        return Err(SocketError::PacketParse("Read request does not contain a mode, but it needs to!".to_string()));
+    }
+
+    let (raw_mode, mode_end) = string_from_buffer(&buf[path_end+1..]);
+
+    if path_end + mode_end >= buf.len() {
+        return Err(SocketError::PacketParse("Mode must be terminated with a null byte!".to_string()));
+    }
+
+    let mode = match raw_mode.to_lowercase().as_str() {
+        "netascii" => FileMode::NetAscii,
+        "octet" => FileMode::Octet,
+        "mail" => FileMode::Mail,
+        _ => return Err(SocketError::PacketParse(format!("Unknown file mode: '{raw_mode}'")))
+    };
+
+    Ok((path, mode))
+}
+
+fn parse_read_req(buf: &[u8]) -> TftpResult<Packet> {
+    let (path, mode) = parse_path_and_mode(&buf[2..])?;
+    Ok(Packet::ReadReq { path, mode })
+}
+
+fn parse_write_req(buf: &[u8]) -> TftpResult<Packet> {
+    let (path, mode) = parse_path_and_mode(&buf[2..])?;
+    Ok(Packet::WriteReq { path, mode })
+}
+
+fn parse_data(buf: &[u8]) -> TftpResult<Packet> {
+    let block = u16_from_buffer(&buf[2..4]);
+    Ok(Packet::Data { block, data: Vec::from(&buf[4..]) })
+}
+
+fn parse_ack(buf: &[u8]) -> TftpResult<Packet> {
+    let block = u16_from_buffer(&buf[2..4]);
+    Ok(Packet::Ack { block })
+}
+
+fn parse_error(buf: &[u8]) -> TftpResult<Packet> {
+    let raw_err = u16_from_buffer(&buf[2..4]);
+    let code = match raw_err {
+        0 => ErrorCode::Undefined,
+        1 => ErrorCode::FileNotFound,
+        2 => ErrorCode::AccessViolation,
+        3 => ErrorCode::DiskFull,
+        4 => ErrorCode::Illegal,
+        5 => ErrorCode::UnknownTid,
+        6 => ErrorCode::FileAlreadyExists,
+        7 => ErrorCode::NoSuchUser,
+        _ => ErrorCode::Undefined,
+    };
+
+    let (message, _) = string_from_buffer(&buf[4..]);
+    Ok(Packet::Error { code, message })
+}
+
+impl Packet {
+    fn parse_from_buf(buf: &[u8]) -> TftpResult<Packet> {
+        match retrieve_op_code(&buf[..2])? {
+            OpCode::Rrq => parse_read_req(&buf),
+            OpCode::Wrq => parse_write_req(&buf),
+            OpCode::Data => parse_data(&buf),
+            OpCode::Ack => parse_ack(&buf),
+            OpCode::Error => parse_error(&buf),
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////
+/// Wrapper around a UDP socket that parses TFTP headers and
+/// returns the packets in a more structured format.
+pub struct TftpSocket {
+    sock: Async<UdpSocket>,
+}
+
+impl TftpSocket {
+    pub fn bind(addr: SocketAddr) -> TftpResult<TftpSocket> {
+        Ok(TftpSocket {
+            sock: Async::<UdpSocket>::bind(addr)?,
+        })
+    }
+
+    pub async fn recv_with_timeout(&mut self, ttl: Duration) -> TftpResult<(Packet, SocketAddr)> {
+        let mut buf = [0; 514];
+        let (total_written, src) = timeout(ttl, self.sock.recv_from(&mut buf)).await??;
+        let packet = Packet::parse_from_buf(&buf[..total_written])?;
+        Ok((packet, src))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_packet_read_req() {
+        let buf = vec![
+            // opcode
+            0x00, 0x01,
+            // path: /path/to/data.txt with terminating nullchar
+            0x2F, 0x70, 0x61, 0x74, 0x68, 0x2F, 0x74, 0x6F, 0x2F, 0x64, 0x61, 0x74, 0x61, 0x2E, 0x74, 0x78, 0x74, 0x00,
+            // mode: mail
+            0x6D, 0x61, 0x69, 0x6C, 0x00];
+
+        let packet = Packet::parse_from_buf(&buf);
+        assert!(packet.is_ok());
+        assert_eq!(packet.unwrap(), Packet::ReadReq { path: "/path/to/data.txt".to_string(), mode: FileMode::Mail });
+    }
+
+    #[test]
+    fn test_packet_write_req() {
+        let buf = vec![
+            // opcode
+            0x00, 0x02,
+            // path: /path/to/data.txt with terminating nullchar
+            0x2F, 0x70, 0x61, 0x74, 0x68, 0x2F, 0x74, 0x6F, 0x2F, 0x64, 0x61, 0x74, 0x61, 0x2E, 0x74, 0x78, 0x74, 0x00,
+            // mode: mail
+            0x6D, 0x61, 0x69, 0x6C, 0x00];
+
+        let packet = Packet::parse_from_buf(&buf);
+        assert!(packet.is_ok());
+        assert_eq!(packet.unwrap(), Packet::WriteReq { path: "/path/to/data.txt".to_string(), mode: FileMode::Mail });
+    }
+
+
+    #[test]
+    fn test_packet_parses_data() {
+        let buf = vec![
+            // opcode
+            0x00, 0x03,
+            // block number
+            0x12, 0x34,
+            // data
+            0xDE, 0xAD, 0xBE, 0xEF];
+
+        let packet = Packet::parse_from_buf(&buf);
+        assert!(packet.is_ok());
+        assert_eq!(packet.unwrap(), Packet::Data { block: 0x1234, data: vec![0xDE, 0xAD, 0xBE, 0xEF] });
+    }
+
+    #[test]
+    fn test_packet_parses_ack() {
+        let buf = vec![0x00, 0x04, 0x10, 0x2f];
+        let packet = Packet::parse_from_buf(&buf);
+        assert!(packet.is_ok());
+        assert_eq!(packet.unwrap(), Packet::Ack { block: 0x102f } );
+    }
+
+    #[test]
+    fn test_packet_parses_error() {
+        let buf = vec![
+            // opcode
+            0x00, 0x05,
+            // Error code
+            0x00, 0x04,
+            // Error message: Illegal!
+            0x49, 0x6C, 0x6C, 0x65, 0x67, 0x61, 0x6C, 0x21];
+
+        let packet = Packet::parse_from_buf(&buf);
+        assert!(packet.is_ok());
+        assert_eq!(packet.unwrap(), Packet::Error { code: ErrorCode::Illegal, message: "Illegal!".to_string() });
+    }
+
+
+}
+
